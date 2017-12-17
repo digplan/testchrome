b364c0014187da626897338e644c0752282eeaae
digital planets <digplan@users.noreply.github.com>
2017-12-17 15:38:47 -0600

    Update and rename testpage.js to main.js

---
+++ main.js
@@ -0,0 +1,27 @@
+const testchrome = url =>new Promise(r=>{
+  require('chrome-remote-interface')(cli=>{
+    cli.once('ready', async ()=>{
+      cli.Page.navigate({url: url})
+      r({
+        get: async url=>{
+          var f = await require('node-fetch')(url)
+          var j = await f.json()
+          return j
+        },
+        evaluate: async x=>{
+         var r = await cli.Runtime.evaluate({expression:x})
+         if(r.result.subtype=='error') throw Error(r.result.description)
+         return r.result.value 
+        }
+      })
+    }) 
+    cli.Network.enable()
+    cli.Page.enable()
+  })
+})
+
+require('chrome-remote-interface')().catch(e=>{
+  console.log('Chrome not started. # chrome --remote-debugging-port=9222 --headless')
+  process.exit()
+})
+module.exports = testchrome
